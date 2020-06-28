fbc120d82122d87741b6f646b8e5b145763750ee
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-06-28 23:49:18 +0300

    Use custom dmenu flags

--- config.def.h
+++ config.def.h
@@ -63,7 +63,24 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+/* static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL }; */
+
+static const char *dmenucmd[] = {
+	"dmenu_run",
+	"-b",
+	"-p", "dmenu%",
+	"-nf", "#F8F8F2",
+	"-nb", "#282A36",
+	"-sb", "#6272A4",
+	"-sf", "#F8F8F2",
+	"-m", dmenumon,
+	"-fn", dmenufont,
+	"-nb", col_gray1,
+	"-nf", col_gray3,
+	"-sb", col_cyan,
+	"-sf", col_gray4,
+	NULL };
+
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
