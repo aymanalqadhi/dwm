406f8f4ee32e59746a18f520c992bb8911815377
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-06-29 15:00:53 +0300

    Add sprial layout key binding

--- config.def.h
+++ config.def.h
@@ -1,4 +1,5 @@
 #include "tcl.c"
+#include "fibonacci.c"
 
 /* See LICENSE file for copyright and license details. */
 
@@ -52,6 +53,7 @@ static const Layout layouts[] = {
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 	{ "|||",      tcl },
+    { "(@)",      spiral },
 };
 
 /* key definitions */
@@ -104,7 +106,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_r,      setlayout,      {.v = &layouts[3]} },
+	{ MODKEY,                       XK_c,      setlayout,      {.v = &layouts[3]} },
+	{ MODKEY,                       XK_r,      setlayout,      {.v = &layouts[4]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
